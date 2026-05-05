9c87b4d253c1a8903d15f8f50a973bc6a8703a10
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-05-05 23:29:38 +0300

    Fix a layout bug where on hover item positions would shift

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -34,7 +34,7 @@ export default function Item({ item }: ItemProps) {
   return (
     <li
       key={item.id}
-      className="flex flex-col justify-around gap-3 items-center my-2 bg-[#FFFFFF] rounded-lg hover:border-1 border-gray-400"
+      className="flex flex-col justify-around gap-3 items-center my-2 bg-[#FFFFFF] rounded-lg border border-transparent hover:border-gray-400 transition"
     >
       <div className="rounded-md p-10 w-70 h-70">
         <img src={item.image} alt="" className="w-full h-full object-contain" />
