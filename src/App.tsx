55388210900deb53746de39ef66f8f6611b0f9f4
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-05-08 23:30:29 +0300

    Create cartQuantity state and create navProps interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Header from './components/Header';
 import Nav from './components/Nav';
+import { useState } from 'react';
 import { Outlet } from 'react-router';
 
 function App() {
+  const [cartQuantity, changeCartQuantity] = useState(0);
+
   return (
     <div className="bg-[#FFFFFF] h-[100%] py-[1vw] px-[2vw] text-white rounded-lg">
       <div className="flex">
         <Header />
-        <Nav />
+        <Nav quantity={cartQuantity} />
       </div>
       <Outlet />
     </div>
